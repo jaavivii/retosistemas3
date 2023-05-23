ca30c7508487b3f38c6a493c68d098146f6abaa3
jaavivii <depandojavier@gmail.com>
2023-05-23 20:24:56 +0200

    Subida automatica de examen

---
+++ add_githun_repo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+GITHUB_USERNAME="jaavivii"
+REPO_NAME="$retosistemas"
+FOLDER_PATH="/home/almi/Escritorio/examen"
+
+cd "$FOLDER_PATH"
+
+git init
+
+git add .
+
+git commit -m "Subida automatica de examen"
+
+git remote add origin "https://github.com/jaavivii/retosistemas.git"
+
+git push -u origin master
