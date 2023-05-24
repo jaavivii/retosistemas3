69556173d1d34c4c308ff23b0101b8d9cefa0e7c
jaavivii <depandojavier@gmail.com>
2023-05-24 20:32:14 +0200

    Commit automático al iniciar sesión

--- add_githun_repo.sh
+++ add_githun_repo.sh
@@ -7,6 +7,10 @@ cd /home/almi/Escritorio/examen
 git add .
 git commit -m "Commit automático al iniciar sesión"
 
+# Configura el origen remoto utilizando la URL SSH de tu repositorio
+git remote set-url origin git@github.com:usuario/repositorio.git
+
 # Realiza el push al repositorio remoto
 git push -u origin main
+
 
